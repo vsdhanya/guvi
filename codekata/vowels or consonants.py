fe28c79ac7a4924def29d180d50dc32fbb514037
vsdhanya <51656014+vsdhanya@users.noreply.github.com>
2019-06-12 14:33:28 +0530

    Create vowels or consonants.py

---
+++ codekata/vowels or consonants.py
@@ -0,0 +1,9 @@
+given_alpha=input()
+vowels=['a','A','e','E','i','I','o','O','u','U']
+consonants=['b','B','c','C','d','D','f','F','g','G','h','H','j','J','k','K','l','L','m','M','n','N','p','P','q','Q','r','R','s','S','t','T','v','V','w','W','x','X','y','Y','z','Z']
+if given_alpha in vowels:
+  print("vowel")
+elif given_alpha in consonants:
+  print("consonant")
+else:
+  print("invalid")
