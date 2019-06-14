7da0b218e96f559b767643940ea3b08859fd1ea1
vsdhanya <51656014+vsdhanya@users.noreply.github.com>
2019-06-14 11:17:12 +0530

    Update count number of special characters.py

--- codekata/count number of special characters.py
+++ codekata/count number of special characters.py
@@ -1,6 +1,6 @@
 word=input()
 count=0
 for i in word:
-  if i.isdigit()!=True and i.isalpha()!=True and i!=' ' :
+  if i.isdigit()!=True and i.isalpha()!=True and i.isspace()!=True:
     count=count+1
 print(count)
