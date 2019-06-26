dca5dd35c792d60e0b78ea397b871bb9d1956061
vsdhanya <51656014+vsdhanya@users.noreply.github.com>
2019-06-26 12:26:05 +0530

    Update perfect square or not.py

--- codekata/perfect square or not.py
+++ codekata/perfect square or not.py
@@ -1,10 +1,9 @@
 num1,num2=map(int,input().split())
 num=num1*num2
 ans=0
-if num>0:
-  while ans*ans<num:
-    ans=ans+1
-  if ans*ans==num:
-    print("yes")
-  else:
+while ans*ans<num:
+  ans=ans+1
+if ans*ans==num:
+  print("yes")
+else:
     print("no")
