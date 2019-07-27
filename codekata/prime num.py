3b0f7a82d2bd17dcc5546c48fd593c6996224357
vsdhanya <51656014+vsdhanya@users.noreply.github.com>
2019-07-27 22:20:35 +0530

    Update prime num.py

--- codekata/prime num.py
+++ codekata/prime num.py
@@ -1,10 +1,13 @@
 num=int(input())
-for i in range(1,num):
-  if i>1:
-    for j in range(2,i):
-      if(i%j==0):
-        break
+if num<=2:
+  print("0")
+elif num>2:
+  for i in range(2,num):
+    if i>1:
+      for j in range(2,i):
+        if(i%j==0):
+          break
+      else:
+        print(i,end=" ")
     else:
-      print(i,end=" ")
-  else:
-    print("0")
+      break
